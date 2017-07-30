d308807c827061ee1652d47bf46af8c786be7fc6
Michael Hottman <hottmanmichael@gmail.com>
2017-07-30 13:55:08 -0600

    knex

--- migrations/20170728193247_add_hidden_to_images.js
+++ migrations/20170728193247_add_hidden_to_images.js
@@ -2,11 +2,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.table('images', function(table) {
     table.boolean('hidden');
-  };
+  });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.table('images', function(table) {
     table.dropColumn('hidden');
-  };
+  });
 };

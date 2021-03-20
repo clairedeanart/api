fcd9019c9b3a16dce12fa8ea5bc3ba065da89430
Michael Hottman <hottmanmichael@gmail.com>
2021-03-20 14:49:44 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,8 @@ module.exports = {
 
     production: {
         client: 'pg',
-        connection: `${process.env.DATABASE_URL}?ssl=true`
+        // connection: `${process.env.DATABASE_URL}?ssl=true`
+        connection: process.env.DATABASE_URL,
     }
 
 };

32edcc7f918a422ef34516a50cf1226af532be05
Michael Hottman <hottmanmichael@gmail.com>
2021-03-20 14:56:58 -0600

    ?

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,7 @@ module.exports = {
 
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_URL,
+        connection: process.env.DATABASE_URL + `?ssl=true`,
         ssl: { rejectUnauthorized: false },
     }
 
